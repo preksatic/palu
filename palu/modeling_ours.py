0bdca1f94d14803204d09166540218be8f33b802
Your Name <you@example.com>
2025-07-28 06:06:48 +0000

    fix bug

--- palu/modeling_ours.py
+++ palu/modeling_ours.py
@@ -80,11 +80,11 @@ class LlamaAttention_ours(nn.Module):
         **kwargs,
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
         bsz, q_len, _ = hidden_states.size()
+    
 
-        
+        query_states = self.q_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
 
-        if self.w is not None:
-            query_states = self.q_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        if query_states.shape[-2] == 1:
             key_states = F.linear(hidden_states, self.w, None).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
             value_states = self.v_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
 
@@ -95,18 +95,17 @@ class LlamaAttention_ours(nn.Module):
             query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
 
         else:
-            query_states = self.q_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
             value_states = self.v_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
             kv_seq_len = value_states.shape[-2]
             if past_key_value is not None:
                 kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
             cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
             query_states, _ = apply_rotary_pos_emb(query_states, value_states, cos, sin, position_ids)
-
             w = torch.zeros(self.hidden_size, self.hidden_size, dtype=self.k_proj.weight.dtype, device=hidden_states.device)
             for i in range(self.num_heads):
                 P = self.get_query_subspace(query_states[0][i], self.config.subspace_dim)
                 w[i*self.head_dim:(i+1)*self.head_dim, i*self.head_dim:(i+1)*self.head_dim] = P.T @ P
+
             self.w = w @ self.k_proj.weight.data.to(hidden_states.device)
             key_states = F.linear(hidden_states, self.w, None).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
    
